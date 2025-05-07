35cb199fa8a9fa9bcd7587d1cbf367935cee7270
freniecki <247773@edu.p.lodz.pl>
2025-05-07 15:28:55 +0200

    Initial commit

---
+++ src/main/java/pl/freniecki/siitask/SiitaskApplication.java
@@ -0,0 +1,13 @@
+package pl.freniecki.siitask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiitaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiitaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/freniecki/siitask/SiitaskApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.freniecki.siitask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiitaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
