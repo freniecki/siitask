20f7c0c490f9a66df3d90777c0d71a46e3c8dc2a
freniecki <247773@edu.p.lodz.pl>
2025-05-09 17:52:56 +0200

    custom search

--- src/main/java/pl/freniecki/siitask/repository/EventAccountRepository.java
+++ src/main/java/pl/freniecki/siitask/repository/EventAccountRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.freniecki.siitask.model.EventAccount;
 
+import java.util.Optional;
+
 @Repository
 public interface EventAccountRepository extends JpaRepository<EventAccount, Long> {
+    Optional<EventAccount> findByEventId(Long eventId);
 }
