17ac696aed7c7d23eda7e3a1ee8a493ae5bd25f3
freniecki <247773@edu.p.lodz.pl>
2025-05-09 17:53:49 +0200

    adjustments for db and access

--- src/main/java/pl/freniecki/siitask/model/EventAccount.java
+++ src/main/java/pl/freniecki/siitask/model/EventAccount.java
@@ -1,19 +1,24 @@
 package pl.freniecki.siitask.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class EventAccount {
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column (unique = true, nullable = false)
     private Long eventId;
 
     private Currency currency;
